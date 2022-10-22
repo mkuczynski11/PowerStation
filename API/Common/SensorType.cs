e13b76eb4d91c96e313265b4f6288612341b49b9
Michal Flakowski <mflakowski.contact@gmail.com>
2022-10-22 17:31:41 +0200

    Unify common settings

---
+++ API/Common/SensorType.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public static class SensorType
+    {
+        public static string CORE_TEMPERATURE = "coreTemperature";
+        public static string POWER_GENERATED = "powerGenerated";
+        public static string WATER_USAGE = "waterUsage";
+        public static string TURBINE_RPM = "turbineRpm";
+    }
+}
---
+++ Common/QueueNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public class QueueNames
+    {
+        public static string CORE_TEMPERATURE = "core-temperature";
+        public static string POWER_GENERATED = "power-generated";
+        public static string WATER_USAGE = "water-usage";
+        public static string TURBINE_RPM = "turbine-rpm";
+    }
+}
